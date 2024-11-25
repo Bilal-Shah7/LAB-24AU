abcfbf30b00bdbea6dda44aa3ff908f42686cc9b
Bilal Shah <bilal.shah78@yahoo.com>
2024-11-25 18:16:28 +0500

    Update Task01.cpp

--- Arrays/Task01.cpp
+++ Arrays/Task01.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 using namespace std;
 
-//use of ararys
+//use of arrays
 // printing 10-100 using for and while loops.
 int main (){
 int a[10] = {10,20,30,40,50,60,70,80,90,100};
