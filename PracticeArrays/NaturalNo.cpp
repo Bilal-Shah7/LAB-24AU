3bc076f838b41ce08a005ff33149766df24f2ad6
Bilal Shah <bilal.shah78@yahoo.com>
2024-11-26 00:33:14 +0500

    Practice

---
+++ PracticeArrays/NaturalNo.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+int n;
+cout << "Enter limit of natural number of which you want the square : ";
+cin >>n;
+
+int arr[n];
+int c = 0;
+cout << "The first ("<<n <<") Natural numbers are : ";
+
+for (int i = 0; i<n ; i++){
+     c++;
+     arr[i]= c;
+    cout << arr[i]<< " ";
+}
+
+cout << "The squares of the first ("<<n<<") Natural numbers are : ";
+
+for (int i = 0; i<n; i++){
+    cout << arr[i]*arr[i]<<" ";
+}
+
+}
